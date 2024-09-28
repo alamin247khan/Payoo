de529d984a8e5156f8cb4442d003511ca0e0d3fc
Al Amin Khan <167293333+alamin247khan@users.noreply.github.com>
2024-09-28 04:22:17 -0600

    update

--- home.js
+++ home.js
@@ -1,6 +1,18 @@
 // add money to the account
 
-document.getElementById('btn-add-money')
-.addEventListener('click' function(){
-    console.log('money add btn clicked');
-});
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Money add button clicked");
+    const amountInput = document.getElementById("amount-input");
+    const amount = amountInput.value;
+    if (amount > 0) {
+      console.log("Adding " + amount + " units to the balance");
+    } else {
+      alert("Please enter a valid amount.");
+    }
+
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+  });
